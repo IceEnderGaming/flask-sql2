fd6d88ee802cf917a1a8f2b97ffce4b1b2d6b32a
IceEnderGaming <karta8950@gmail.com>
2024-03-10 11:02:18 +0700

    class-1

---
+++ login.py
@@ -0,0 +1,58 @@
+from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('base.html')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_session.global_init("db/users.db")
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_session.global_init("db/users.db")
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+app.run(host='127.0.0.1', port=5000, debug=True)
