c5cc6a0182ba7e060cc413525ed72a9ea83866ee
IceEnderGaming <karta8950@gmail.com>
2024-03-10 11:26:26 +0700

    class-3

--- data/jobs.py
+++ data/jobs.py
@@ -7,14 +7,9 @@ from sqlalchemy import orm
 class Jobs(SqlAlchemyBase):
     __tablename__ = 'jobs'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                primary_key=True, autoincrement=True)
     team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"), nullable=True)
-    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    jobs = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    start_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
-    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
-
-    def __repr__(self):
-        return f'<Job> {self.job}'
--- main.py
+++ main.py
@@ -1,31 +1,62 @@
-from flask import Flask, request, make_response, session
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-@app.route("/cookie_test")
-def cookie_test():
-    visits_count = int(request.cookies.get("visits_count", 0))
-    if visits_count:
-        res = make_response(
-            f"Вы пришли на эту страницу {visits_count + 1} раз")
-        res.set_cookie("visits_count", str(visits_count + 1),
-                       max_age=60 * 60 * 24 * 365 * 2)
-    else:
-        res = make_response(
-            "Вы пришли на эту страницу в первый раз за последние 2 года")
-        res.set_cookie("visits_count", '1',
-                       max_age=60 * 60 * 24 * 365 * 2)
-    return res
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    db_session.global_init("db/users.db")
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).filter(Jobs.user_id == current_user.id).first()
+    jobs = jobs.jobs
+    return render_template('index.html', jobs=jobs.split(", "))
 
 
-@app.route("/session_test")
-def session_test():
-    visits_count = session.get('visits_count', 0)
-    session['visits_count'] = visits_count + 1
-    return make_response(
-        f"Вы пришли на эту страницу {visits_count + 1} раз")
+@login_manager.user_loader
+def load_user(user_id):
+    db_session.global_init("db/users.db")
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
-app.run(host="127.0.0.1", port=5000, debug=True)
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_session.global_init("db/users.db")
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+
+app.run(host='127.0.0.1', port=5000, debug=True)
